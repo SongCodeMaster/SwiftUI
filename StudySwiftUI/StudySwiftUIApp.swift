aacc8574eaac170947886ddb5b22f74e8bba87ff
SongCodeMaster <lssyl_@naver.com>
2025-03-06 17:38:50 +0900

    Initial Commit

---
+++ StudySwiftUI/StudySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudySwiftUIApp.swift
+//  StudySwiftUI
+//
+//  Created by 송승윤 on 3/6/25.
+//
+
+import SwiftUI
+
+@main
+struct StudySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
